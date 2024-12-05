700d7d06e60b022a05f935f27278ab7c7de50871
shubham sharma <shubhamtech004@gmail.com>
2024-12-05 15:07:44 +0530

    user tabel ui done with delete action

--- src/Pages/AdminUser.jsx
+++ src/Pages/AdminUser.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../Store/Auth'
-
+import '../table.css'
+import { toast } from 'react-toastify';
 function AdminUser() {
    const {authorizationToken} = useAuth();
-   const [users, setusers] = useState([])
+   const [userin, setusers] = useState([])
    const getAlluserdata = async()=>{
       try {
         // admin/users
@@ -22,6 +23,30 @@ function AdminUser() {
       }
    }
 
+   const deleteUser = async(id) =>{
+       
+    try {
+      // admin/users
+          const response = await fetch(`http://localhost:5000/api/v1/admin/users/delete/${id}`,{
+            method:"DELETE",
+            headers:{
+              Authorization:authorizationToken
+            }
+          })
+          const data = await response.json();
+           console.log(`user after delete : ${data}`);
+           if(response.ok){
+            getAlluserdata();
+            toast.info("user deleted successful");
+           }
+          
+    } catch (error) {
+        console.log(error);
+        
+    }
+   }
+
+
    useEffect(() =>{
     getAlluserdata();
    },[])
@@ -32,16 +57,35 @@ function AdminUser() {
           <h1>Admin user Data</h1>
         </div>
         <div className='conatiner admin-users'>
-        {/* {users.map((curUser,index)=>{
-           return <>
-                 <h2 key={index}>{curUser.fullname} {curUser.email}</h2>
-                 
-           </>
-      })} */}
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        {userin.map((curUser,index)=>{
+                            return <tr key={index}>
+                                     <td>{curUser.fullname}</td>
+                                     <td>{curUser.email}</td>
+                                     <td>{curUser.phone}</td>
+                                     <td>Edit</td>
+                                     <td>
+                                       <button onClick={() => deleteUser(curUser._id)}>Delete</button>
+                                     </td>
+                            </tr>
+                        })}
+                    </tbody>
+                  </table>
         </div>
       </section>
+       
       
-      
+
       </>
   )
 }
